e10367a1ff693f3a5955181ce567816eef3bf69c
WisdomFusion <WisdomFusion@gmail.com>
2018-09-04 15:17:24 +0800

    fix

--- lib/MicroMis/Model/Base.pm
+++ lib/MicroMis/Model/Base.pm
@@ -5,18 +5,10 @@ use warnings;
 
 use Mojo::Base -base;
 
-# $res = $coll->insert_one( $document );
-# $res = $coll->insert_one( $document, $options );
-# $id  = $res->inserted_id;
-sub add {
-  my ( $class, $document ) = @_;
-  my $coll = MicroMis::Model->db->get_collection( $class->collection );
-  $coll->insert_one( $document );
-}
-
-# $cursor = $coll->find( $filter );
-# $cursor = $coll->find( $filter, $options );
-# $cursor = $coll->find({ i => { '$gt' => 42 } }, {limit => 20});
+# MongoDB:
+#   $cursor = $coll->find( $filter );
+#   $cursor = $coll->find( $filter, $options );
+#   $cursor = $coll->find({ i => { '$gt' => 42 } }, {limit => 20});
 sub all {
   my $class   = shift;
   my $filter  = shift || { };
@@ -32,8 +24,9 @@ sub all {
   $coll->find( $filter, $options )->fields( $hidden_fields )->all;
 }
 
-# $doc = $collection->find_one( $filter, $projection );
-# $doc = $collection->find_one( $filter, $projection, $options );
+# MongoDB:
+#   $doc = $collection->find_one( $filter, $projection );
+#   $doc = $collection->find_one( $filter, $projection, $options );
 sub find_one {
   my $class  = shift;
   my $filter = shift || { };
@@ -48,9 +41,10 @@ sub find_one {
   $coll->find_one( $filter, $hidden_fields );
 }
 
-# $doc = $collection->find_id( $id );
-# $doc = $collection->find_id( $id, $projection );
-# $doc = $collection->find_id( $id, $projection, $options );
+# MongoDB:
+#   $doc = $collection->find_id( $id );
+#   $doc = $collection->find_id( $id, $projection );
+#   $doc = $collection->find_id( $id, $projection, $options );
 sub find_id {
   my ( $class, $oid ) = @_;
   return undef unless $oid;
@@ -65,9 +59,20 @@ sub find_id {
   $coll->find_id( $oid, $hidden_fields );
 }
 
-# $res = $coll->delete_many( $filter );
-# $res = $coll->delete_many( { name => "Larry" } );
-# $res = $coll->delete_many( $filter, { collation => { locale => "en_US" } } );
+# MongoDB:
+#   $res = $coll->insert_one( $document );
+#   $res = $coll->insert_one( $document, $options );
+#   $id  = $res->inserted_id;
+sub add {
+  my ( $class, $document ) = @_;
+  my $coll = MicroMis::Model->db->get_collection( $class->collection );
+  $coll->insert_one( $document );
+}
+
+# MongoDB:
+#   $res = $coll->delete_many( $filter );
+#   $res = $coll->delete_many( { name => "Larry" } );
+#   $res = $coll->delete_many( $filter, { collation => { locale => "en_US" } } );
 sub delete_many {
   my ( $class, $filter ) = @_;
   return undef unless $filter;
@@ -76,9 +81,10 @@ sub delete_many {
   $coll->delete_many( $filter );
 }
 
-# $res = $coll->delete_one( $filter );
-# $res = $coll->delete_one( { _id => $id } );
-# $res = $coll->delete_one( $filter, { collation => { locale => "en_US" } } );
+# MongoDB:
+#   $res = $coll->delete_one( $filter );
+#   $res = $coll->delete_one( { _id => $id } );
+#   $res = $coll->delete_one( $filter, { collation => { locale => "en_US" } } );
 sub delete_one {
   my ( $class, $filter ) = @_;
   return undef unless $filter;
@@ -87,25 +93,46 @@ sub delete_one {
   $coll->delete_one( $filter );
 }
 
-# $res = $coll->update_one( $filter, $update );
-# $res = $coll->update_one( $filter, $update, { upsert => 1 } );
+# MongoDB:
+#   $res = $coll->update_one( $filter, $update );
+#   $res = $coll->update_one( $filter, $update, { upsert => 1 } );
 sub update {
   my ( $class, $filter, $update ) = @_;
   my $coll = MicroMis::Model->db->get_collection( $class->collection );
   $coll->update_one( $filter, $update );
 }
 
-# $count = $coll->count_documents( $filter );
-# $count = $coll->count_documents( $filter, $options );
+# MongoDB:
+#   $count = $coll->count_documents( $filter );
+#   $count = $coll->count_documents( $filter, $options );
 sub count {
   my ( $class, $criteria ) = @_;
   my $coll = MicroMis::Model->db->get_collection( $class->collection );
   $coll->count_documents( $criteria );
 }
 
+# get a collection
 sub coll {
   my $class = shift;
   MicroMis::Model->db->get_collection( $class->collection );
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+MicroMis::Model::Base - The base class of models
+
+=head1 SYNOPSIS
+
+  my $user_model = MicroMis::Model::User->new;
+  
+  @users = $user_model->all;
+
+=cut
